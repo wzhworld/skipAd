cc7ec6ace634ad6c5d72d6c9d7451a5a47b5d4a0
wzhworld <604835748@qq.com>
2023-11-25 14:17:37 +0800

    # 修改点击处理逻辑

--- app/src/main/java/com/zh1/skipsplashad/service/MyAccessibilityService.kt
+++ app/src/main/java/com/zh1/skipsplashad/service/MyAccessibilityService.kt
@@ -1,10 +1,12 @@
 package com.zh1.skipsplashad.service
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.graphics.Rect
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import com.zh1.skipsplashad.utils.LogTool
-import com.zh1.skipsplashad.utils.ToastUtils
 
 //https://mp.weixin.qq.com/s?__biz=MzA5MzI3NjE2MA==&mid=2650278067&idx=1&sn=a738ff575526ea5c019f05a4e2aeb797&chksm=886cf9dcbf1b70ca989ca14a097a0fcfca762ca3447349acc853b155610bd8cb3001aec1200c&scene=27
 class MyAccessibilityService : AccessibilityService() {
@@ -15,16 +17,50 @@ class MyAccessibilityService : AccessibilityService() {
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         event?.let {
-            // 如果查找包含跳过按钮的结点列表不为空，取第一个，然后输出
-            var nodeInfo = getCurrentRootNode()?.findAccessibilityNodeInfosByText("跳过")
-                .takeUnless { it.isNullOrEmpty() }?.get(0)
-                nodeInfo?.let{
-                    if (it.isClickable) {
-                        it.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        ToastUtils.toast(this@MyAccessibilityService, "已为您跳过广告")
-                    }
-                }
+            // 如果查找包含跳过按钮的结点列表不为空
+            var listOfRect = handleNode(getCurrentRootNode())
+            listOfRect ?: return
+            for (rect in listOfRect) {
+                click(this@MyAccessibilityService, rect)
+            }
         }
+    }
+
+    fun handleNode(node: AccessibilityNodeInfo?): List<Rect>? {
+        val nodes = node?.findAccessibilityNodeInfosByText("跳过")
+        if (null == nodes || nodes.isEmpty()){
+            return null
+        }
+        LogTool.d("TextNodeHandler node$nodes");
+        //
+        val listOfRect = nodes.map {
+            val rect = Rect()
+            it.getBoundsInScreen(rect)
+            rect
+        }
+        nodes.forEach { it.recycle() }
+
+        return listOfRect
+    }
+
+    private fun click(accessibilityService: AccessibilityService, rect: Rect) {
+        val path = Path()
+        path.moveTo(rect.centerX().toFloat(), rect.centerY().toFloat())
+        path.lineTo(rect.centerX().toFloat(), rect.centerY().toFloat())
+
+        val gesture = GestureDescription.Builder()
+            .addStroke(GestureDescription.StrokeDescription(path, 0, 1))
+            .build()
+
+        accessibilityService.dispatchGesture(
+            gesture,
+            object : AccessibilityService.GestureResultCallback() {
+                override fun onCompleted(gestureDescription: GestureDescription) {
+                    super.onCompleted(gestureDescription)
+                }
+            },
+            null
+        )
     }
 
     private fun getCurrentRootNode() = try {
--- app/src/main/java/com/zh1/skipsplashad/utils/LogTool.kt
+++ app/src/main/java/com/zh1/skipsplashad/utils/LogTool.kt
@@ -3,7 +3,7 @@ package com.zh1.skipsplashad.utils
 import android.util.Log
 
 object LogTool {
-    val TAG: String = "zhizhi"
+    val TAG: String = "SkipSplashAd"
 
     fun d(str: String) {
         Log.e(TAG, str)
